58e04def0f6c6f77dada3795c4b0dacd1f36ac2e
Urbattt <80606638+Urbattt@users.noreply.github.com>
2022-05-07 01:53:10 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+noseX=0;
+noseY=0;
+rightwristX=0;
+leftwristX=0;
+difference=0;
+
+
 function setup(){
     canvas= createCanvas(500,500);
     canvas.position(500,150);
@@ -19,13 +26,23 @@ function setup(){
     function gotPoses(results){
     if(results.length>0){
     console.log(results);
-    
+    noseX=results[0].pose.nose.x;
+noseY=results[0].pose.nose.y;
+console.log("noseX="+noseX+"noseY="+noseY);
+leftwristX=results[0].pose.leftWrist.x;
+rightwristX=results[0].pose.rightWrist.x;
+difference=floor(leftwristX - rightwristX);
+console.log("leftwrist="+leftwristX+"rightwrist="+rightwristX+"difference="+difference);
     }
     
     }
     
     function draw(){
     background('#DC143C');
-    
+    fill('#FFFF00');
+stroke('#0000FF');
+text("Dylan",noseX,noseY,);
+textSize(difference);
+document.getElementById('square').innerHTML="The length of your name is "+difference+"px";
     }
     
